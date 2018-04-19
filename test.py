0bc5d930e7d8eaf111ea4130932a7d6415f14efe
Qing Guo <qguo43@gmail.com>
2018-04-18 21:46:26 -0400

    add some functions

--- test.py
+++ test.py
@@ -3,9 +3,40 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get("https://www.glassdoor.com/index.htm")
-input_area = driver.find_element_by_name("sc.keyword")
-input_area.send_keys("data analyst")
-input_area.send_keys(Keys.RETURN)
+assert "Glassdoor" in driver.title
+
+# type job and location
+input_job = driver.find_element_by_name("sc.keyword")
+input_job.clear()
+input_job.send_keys("data analyst")
+
+input_location = driver.find_element_by_id("LocationSearch")
+input_location.clear()
+input_location.send_keys("New York")
+input_location.submit()
+assert "No results found." not in driver.page_source
+
+#search for individual description listed by companies
+all_companies = driver.find_elements_by_class_name("jl")
+
+company_list = []
+job_description = []
+for company in all_companies:
+    company_list.append(company.text)
+    company.click()
+    driver.implicitly_wait(10)
+    try:
+        descrip = driver.find_element_by_class_name("jobDescriptionContent")
+        job_description.append(descrip.text)
+    except:
+        job_description.append("N/A")
+
+
+
+
+
+
+
 
 
 
